4bfdeaf46bf1395acc4b831526dd26782a35d0d1
Neil Herald <neil.herald@gmail.com>
2020-12-18 10:19:08 +0000

    Add helper to auth against a firestore project

---
+++ src/main/java/nherald/indigo/store/firebase/FirebaseAuth.java
@@ -0,0 +1,50 @@
+package nherald.indigo.store.firebase;
+
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class FirebaseAuth
+{
+    /**
+     * Authorises against a firestore project. Should only be done once, before any firestore
+     * requests are made
+     * @param projectId GCP project id that contains the firestore database
+     * @throws FirebaseAuthException if there was an error reading credentials
+     */
+    public void auth(String projectId)
+    {
+        // One-time init of Firestore
+        final GoogleCredentials credentials = getGoogleCredentials();
+        final FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(credentials)
+            .setProjectId(projectId)
+            .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+
+    private static GoogleCredentials getGoogleCredentials()
+    {
+        try
+        {
+            return GoogleCredentials.getApplicationDefault();
+        }
+        catch (IOException ex)
+        {
+            throw new FirebaseAuthException("Error getting google credentials", ex);
+        }
+    }
+
+    public static class FirebaseAuthException extends RuntimeException
+    {
+        private static final long serialVersionUID = 1L;
+
+        public FirebaseAuthException(String message, Throwable t)
+        {
+            super(message, t);
+        }
+    }
+}
